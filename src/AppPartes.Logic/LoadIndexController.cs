96aa68b0e1414d8ebd1f5d127cda90f74160f687
JavitronAld <jsanmartin@aldakin.com>
2020-04-23 11:43:18 +0200

    a

--- src/AppPartes.Logic/LoadIndexController.cs
+++ src/AppPartes.Logic/LoadIndexController.cs
@@ -21,8 +21,8 @@ namespace AppPartes.Logic
         }
         private async void GetUserDataAsync(int idAldakinUser)
         {
-            var user = await aldakinDbContext.Usuarios.FirstOrDefaultAsync(x => x.Idusuario == idAldakinUser && x.CodEnt == x.CodEntO);
-            //var user =  aldakinDbContext.Usuarios.FirstOrDefault(x => x.Idusuario == idAldakinUser && x.CodEnt == x.CodEntO);
+            //var user = await aldakinDbContext.Usuarios.FirstOrDefaultAsync(x => x.Idusuario == idAldakinUser && x.CodEnt == x.CodEntO);
+            var user = aldakinDbContext.Usuarios.FirstOrDefault(x => x.Idusuario == idAldakinUser && x.CodEnt == x.CodEntO);
             if (!(user is null))
             {
                 strUserName = user.Nombrecompleto.ToString();
